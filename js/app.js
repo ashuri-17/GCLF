171c12b321effa127cddc3005a532ed23dc166bc
Maliga-17 <erenmaliga17@gmail.com>
2026-05-03 00:33:18 +0800

    Disable Firestore listeners completely to stop data overwrite loops

--- js/app.js
+++ js/app.js
@@ -744,43 +744,9 @@ async function saveAllDataToFirestore() {
 }
 
 function setupRealTimeListeners() {
-  // Unsubscribe existing listeners first
-  for (var i = 0; i < unsubscribers.length; i++) {
-    if (typeof unsubscribers[i] === "function") unsubscribers[i]();
-  }
-  unsubscribers = [];
-
-  // Listen to found items
-  var unsub1 = listenToCollection("foundItems", function(data) {
-    itemsData = data;
-    if (typeof renderItems === "function") renderItems();
-    if (typeof updateStudentStats === "function") updateStudentStats();
-    if (typeof updateAdminStats === "function") updateAdminStats();
-  });
-  unsubscribers.push(unsub1);
-
-  // Listen to claims
-  var unsub2 = listenToCollection("claims", function(data) {
-    allClaims = data;
-    rebuildMyClaimsByEmail();
-    if (typeof renderAdminClaims === "function") renderAdminClaims();
-  });
-  unsubscribers.push(unsub2);
-
-  // Listen to lost reports
-  var unsub3 = listenToCollection("lostReports", function(data) {
-    lostReports = data;
-    if (typeof renderLostReportsList === "function") renderLostReportsList();
-    if (typeof renderLostMatches === "function") renderLostMatches();
-  });
-  unsubscribers.push(unsub3);
-
-  // Listen to pending found reports
-  var unsub4 = listenToCollection("pendingFoundReports", function(data) {
-    pendingFoundReports = data;
-    if (typeof renderAdminFoundReportsList === "function") renderAdminFoundReportsList();
-  });
-  unsubscribers.push(unsub4);
+  // DISABLED: Firestore listeners cause data overwrite issues
+  // Will re-enable once Firestore data is cleaned up
+  return;
 }
 
 async function doLogin() {
